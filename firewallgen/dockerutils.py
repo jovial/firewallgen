13c0b2a852bcefdc81645c766357d42341eca6c6
Will Szumski <will@stackhpc.com>
2019-03-27 10:20:47 +0000

    fixups

--- firewallgen/dockerutils.py
+++ firewallgen/dockerutils.py
@@ -51,11 +51,11 @@ def pid_to_name(pid):
 def _lookup_bridge_name(network):
     client = docker.from_env()
     try:
-        network = client.network.get(network)
+        network = client.networks.get(network)
     except docker.errors.NotFound as _:
         logger.info("network %s not found" % network)
         return
-    options = network["Options"]
+    options = network.attrs["Options"]
     if "com.docker.network.bridge.name" not in options:
         return
     return options["com.docker.network.bridge.name"]
@@ -63,29 +63,47 @@ def _lookup_bridge_name(network):
 
 def _parse_port(port_proto_str):
     split = port_proto_str.split("/")
-    port = split[0]
+    port = int(split[0])
     proto = split[1]
     return port, proto
 
 
-def get_network_meta(pid):
-    container = find_container(pid)
-    # This port could be restricted to one of the networks - we don't
-    # handle that yet
-    ports = container.attrs["NetworkSettings"]["Ports"]
-    if not ports:
-        return
-    networks = container.attrs["NetworkSettings"]["Networks"]
-    bridges = set()
-    for network in networks:
-        bridge = _lookup_bridge_name(network["NetworkID"])
-        if not bridge:
+def get_docker_bindings():
+    client = docker.from_env()
+    containers = client.containers.list()
+    result = []
+    for container in containers:
+        # This port could be restricted to one of the networks - we don't
+        # handle that yet
+        ports = container.attrs["NetworkSettings"]["Ports"]
+        if not ports:
             continue
-        bridges.add(network)
-    bridges = list(bridges)
-    return [
-        {
-            "port": _parse_port(x)[0],
-            "bridges": bridges,
-        } for x in ports
-    ]
+        networks = container.attrs["NetworkSettings"]["Networks"]
+        net_info = []
+        for network_name, network in networks.items():
+            bridge = _lookup_bridge_name(network["NetworkID"])
+            if not bridge:
+                continue
+            net_info.append({
+                "id": network["NetworkID"],
+                "bridge": bridge,
+                "ip": network["IPAddress"],
+                "name": network_name,
+            })
+        for port_proto in ports:
+            # port_proto = "5000/tcp"
+            port, proto = _parse_port(port_proto)
+            for binding in ports[port_proto]:
+                current = {
+                    "container": {
+                        "name": container.name,
+                        "id": container.id
+                    },
+                    "port": port,
+                    "proto": proto,
+                    "host_ip": binding["HostIp"],
+                    "host_port": int(binding["HostPort"]),
+                    "net": net_info,
+                }
+                result.append(current)
+    return result
