403720f21a1710977631eb7633e55d52b8a83cd0
Will Szumski <will@stackhpc.com>
2020-02-07 15:51:06 +0000

    Add support for parsing multiple haproxy configs
    This is the case from stein onwards

--- firewallgen/haproxy.py
+++ firewallgen/haproxy.py
@@ -1,19 +1,31 @@
 import re
+import glob
 
 
 def get_service(ip, port, cfg):
     term = "{}:{}".format(ip, port)
-    with open(cfg) as f:
-        lines = f.readlines()
-        addr = None
-        for line in lines:
-            if re.search("^listen", line):
-                addr = line.split()[1]
-            if term in line:
-                return addr
+    try:
+        with open(cfg) as f:
+            lines = f.readlines()
+            addr = None
+            for line in lines:
+                if re.search("^listen", line):
+                    addr = line.split()[1]
+                if term in line:
+                    return addr
+    except IOError:
+        pass
 
 
 def get_hinter(cfg="/etc/kolla/haproxy/haproxy.cfg"):
     def wrapper(*args, **kwargs):
-        return get_service(*args, cfg=cfg, **kwargs)
+        if isinstance(cfg, list):
+            for item in cfg:
+                files = glob.glob(item)
+                for f in files:
+                    hint = get_service(*args, cfg=f, **kwargs)
+                    if hint:
+                        return hint
+        else:
+            return get_service(*args, cfg=cfg, **kwargs)
     return wrapper
